7c8bc16485634622e2536e3b26cb612704e3df62
z1x1ang <2360283295@qq.com>
2025-06-06 17:11:30 +0800

    second commit

--- src/codegen_wizard/registry.py
+++ src/codegen_wizard/registry.py
@@ -1,7 +1,7 @@
 """
 插件注册中心：发现并加载 entry_point = 'codegen_wizard.generators'
 """
-from importlib.metadata import entry_points
+from importlib_metadata import entry_points
 
 def discover_generators() -> dict:
     eps = entry_points(group="codegen_wizard.generators")
